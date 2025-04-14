bfdf6ccc2b28558d6c7e92599a5c04788f36215f
Isaladwy <Isaladwy@gmail.com>
2025-04-14 19:47:21 +0200

    Add person object and display name in MyFirstComponent

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import MyFirstComponent from './components/MyFirstComponent';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/MyFirstComponent.js
+++ src/components/MyFirstComponent.js
@@ -1,6 +1,15 @@
 export default function MyFirstComponent() {
+  const x = 5;
+
+  const person = {
+    name: "Tamim",
+    age: 2,
+    city: "Alexandria",
+  };
+
   return (
     <div>
+      <h1>{person.name}</h1>
       <h1>Hello World!</h1>
       <p>This is my first component.</p>
     </div>
