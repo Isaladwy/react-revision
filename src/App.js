c09222724a0cac1d3fb2e6556d23d874ef3735fc
Isaladwy <Isaladwy@gmail.com>
2025-04-14 21:41:27 +0200

    Add Header component with styling and integrate into App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import MyFirstComponent from './components/MyFirstComponent';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <MyFirstComponent />
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import './Header.css';
+export default function Header() {
+  return (
+    <div className={'header'}>
+      <h1 >Tarmeez Academy</h1>
+    </div>
+  );
+}
