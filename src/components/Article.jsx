ed10941b0f137225de6200ed008be5e5c3a3ad67
Isaladwy <Isaladwy@gmail.com>
2025-04-15 13:40:59 +0200

    Add styling to Article component for improved visual presentation

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,11 +1,11 @@
 function Article() {
   return (
-    <div>
+    <div style={{ boxShadow: '0px 0px 10px gray', margin: '10px' }}>
       <h1>Tamim</h1>
       <h3>Taaladwy@gmail.com</h3>
       <h3>2023</h3>
     </div>
-  )
+  );
 }
 
-export default Article
+export default Article;
