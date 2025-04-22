b46ba6e3b0fd6dd02b542822650840333bf02755
Isaladwy <Isaladwy@gmail.com>
2025-04-22 09:41:13 +0200

    feat(MyForm): add state management for name input

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,21 +1,30 @@
+import { useState } from "react";
+
 function MyForm() {
+  const [nameInput, setNameInput] = useState('');
   return (
     <form>
       <hr />
       <h2>My Form</h2>
       <label>Name: </label>
-      <input type="text" />
+      <input
+        type="text"
+        value={nameInput}
+        onChange={(event) => {
+          setNameInput(event.target.value);
+        }}
+      />
       <br />
       <br />
       <label>Email: </label>
       <input type="email" />
-      
+
       <br />
       <br />
       <button>Submit</button>
       <hr />
     </form>
-  )
+  );
 }
 
-export default MyForm
+export default MyForm;
