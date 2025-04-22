a82a10d8c0a6a24538a2f448f8630d0da77ffdaa
Isaladwy <Isaladwy@gmail.com>
2025-04-22 06:05:17 +0200

    Refactor MyButton component to define handleClick function within the component for better encapsulation

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,9 +1,16 @@
 function MyButton() {
+  let name = 'Tamim';
   return (
     <div>
-      
+      <button onClick={handleClick}>Click Me</button>
     </div>
   )
+}
+
+function handleClick() {
+  let name = 'Tamim';
+  console.log('Hello ' + name);
+  alert('Hello ' + name);
 }
 
 export default MyButton
